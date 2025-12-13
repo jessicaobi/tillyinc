51d0c115f98af9ca186ad72c17d0a07a84bc27bd
jessicaobi <obicjessica@gmail.com>
2025-12-13 18:58:31 -0500

    Server can now run on a port

---
+++ server.js
@@ -0,0 +1,14 @@
+// Import required modules
+const express = require('express');
+const path = require('path');
+// require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
